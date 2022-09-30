9e22c62168a01d7414ff980e42c367c3b6ae7d5b
Arthur Severo <62119941+Sevzera@users.noreply.github.com>
2022-09-30 15:19:11 -0300

    changed print

--- comp_v1_sev/Main.java
+++ comp_v1_sev/Main.java
@@ -8,39 +8,68 @@ public class Main {
     public static void main(String[] args) {
         try {
             Lexer lex = new Lexer("testes/" + args[0] + ".txt");
+            float i = 0;
+            System.out.println(
+                    "-------------------------------------------------------------------------------------------------------------------");
+            System.out.println("TOKENS");
+            System.out.println("------");
             while (true) {
                 Token scanned = lex.scan();
                 if (scanned.tag == Tag.LIT_STRING) {
                     LiteralString token = (LiteralString) scanned;
-                    System.out.println(token.toString());
+                    System.out.print(token.toString());
                 } else if (scanned.tag == Tag.LIT_INT) {
                     LiteralInteger token = (LiteralInteger) scanned;
-                    System.out.println(token.toString());
+                    System.out.print(token.toString());
                 } else if (scanned.tag == Tag.LIT_FLOAT) {
                     LiteralFloat token = (LiteralFloat) scanned;
-                    System.out.println(token.toString());
+                    System.out.print(token.toString());
                 } else if (scanned.tag == Tag.EOF) {
                     break;
                 } else {
-                    System.out.println(scanned.toString());
+                    System.out.print(scanned.toString());
                 }
-                System.out.println("----------------");
+                if (i == 6) {
+                    i = 0;
+                    System.out.println();
+                } else {
+                    i++;
+                    System.out.print("   ");
+                }
             }
+            i = 0;
+            System.out.println();
+            System.out.println(
+                    "-------------------------------------------------------------------------------------------------------------------");
             System.out.println("SYMBOL TABLE");
-            System.out.println("----------------");
+            System.out.println("------------");
             Set<String> lexeme = Lexer.words.keySet();
             for (String l : lexeme) {
-                System.out.println("Token: " + Lexer.words.get(l).toString());
-                System.out.println("Lexeme: " + l);
-                System.out.println("----------------");
+                System.out.print("[Token: " + Lexer.words.get(l).toString() + ", Lexeme: " + l + "]   ");
+                if (i == 3) {
+                    i = 0;
+                    System.out.println();
+                } else {
+                    i++;
+                }
             }
+            i = 0;
+            System.out.println();
+            System.out.println(
+                    "-------------------------------------------------------------------------------------------------------------------");
 
             System.out.println("ERROR REPORT");
-            System.out.println("----------------");
+            System.out.println("------------");
             Set<Token> faultyToken = Lexer.errors.keySet();
             for (Token t : faultyToken) {
-                System.out.println(
+                System.out.print(
                         "ERROR: Pattern " + (char) t.tag + " not recognized in line " + Lexer.errors.get(t));
+                if (i == 6) {
+                    i = 0;
+                    System.out.println();
+                } else {
+                    i++;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
